7eff945d158e33a75b6e7afb8977a22c1d972310
Sourpomelo-Y6 <hi-y6@hotmail.co.jp>
2020-08-19 03:44:56 +0900

    Change Controller part. Corrections to the license notice.

--- WebControll_M5_RoverC.ino
+++ WebControll_M5_RoverC.ino
@@ -1,4 +1,34 @@
-#include <M5StickC.h>
+/*
+ * Copyright (c) 2015, Majenko Technologies
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+ #include <M5StickC.h>
 
 #include <WiFi.h>
 #include <AsyncTCP.h>
@@ -18,6 +48,7 @@ RoverC rover_c(Wire);
 /* set I2C library*/
 #include <Wire.h>
 
+unsigned int g_client_id = -1;
 
 String form ="<!DOCTYPE html>\r\n"
 "<html lang='ja' xml:lang='ja'>\r\n"
@@ -48,20 +79,9 @@ String form ="<!DOCTYPE html>\r\n"
 "      margin: 10px;\r\n"
 "    }\r\n"
 "\r\n"
-"    .powerpnl {\r\n"
-"      line-height: 1px;\r\n"
-"      width: 100px;\r\n"
-"      height: 100px;\r\n"
-"    }\r\n"
-"\r\n"
 "    .operationpnl {\r\n"
 "      line-height: 30px;\r\n"
 "      height: 120px;\r\n"
-"    }\r\n"
-"\r\n"
-"    .timerpnl {\r\n"
-"      line-height: 15px;\r\n"
-"      height: 90px;\r\n"
 "    }\r\n"
 "\r\n"
 "    .child04 {\r\n"
@@ -74,13 +94,6 @@ String form ="<!DOCTYPE html>\r\n"
 "      width: 300px;\r\n"
 "      height: 200px;\r\n"
 "    }\r\n"
-"\r\n"
-"    .configpnl {\r\n"
-"      line-height: 50px;\r\n"
-"      width: 100px;\r\n"
-"      height: 100px;\r\n"
-"    }\r\n"
-"\r\n"
 "    input.checkboxyz{\r\n"
 "      opacity: 0;\r\n"
 "    }\r\n"
@@ -146,20 +159,6 @@ String form ="<!DOCTYPE html>\r\n"
 "    var count_value = 0;\r\n"
 "    var ws = new WebSocket('ws://192.168.4.1/ws');\r\n"
 "\r\n"
-"    function show_led_icon()\r\n"
-"    {\r\n"
-"      for(var i=0;i<4;i++){\r\n"
-"        var target_led = document.getElementById('timerled' + i);\r\n"
-"        if(i < count_value){\r\n"
-"          target_led.style.fill = '#FF0000';\r\n"
-"        }\r\n"
-"        else\r\n"
-"        {\r\n"
-"          target_led.style.fill = '#770000';\r\n"
-"        }\r\n"
-"      }\r\n"
-"    }\r\n"
-"\r\n"
 "    ws.onopen = function() {\r\n"
 "      window.alert('Connected');\r\n"
 "    };\r\n"
@@ -169,113 +168,31 @@ String form ="<!DOCTYPE html>\r\n"
 "      if(remain_sec == -1)\r\n"
 "      {\r\n"
 "        document.getElementById('display').innerHTML  = 'Connected';\r\n"
-"      }else{           \r\n"
-"        document.getElementById('display').innerHTML  = 'remain: ' + evt.data + '[sec]';\r\n"
-"\r\n"
-"        if(remain_sec == 0){\r\n"
-"          count_value = 0;\r\n"
-"        } else {\r\n"
-"          count_value = parseInt(remain_sec/60 + 1);\r\n"
-"        }\r\n"
-"\r\n"
-"        show_led_icon();\r\n"
-"\r\n"
 "      }\r\n"
 "    };\r\n"
-"\r\n"
-"    window.onload = function() {\r\n"
-" \r\n"
-"      var count_up_btn = document.getElementById('btn_count_up');\r\n"
-"      \r\n"
-"      var count_down_btn = document.getElementById('btn_count_down');\r\n"
-"      count_down_btn.onclick = function (){\r\n"
-"        if(count_value > 0){\r\n"
-"          count_value -= 1;\r\n"
-"\r\n"
-"          show_led_icon();\r\n"
-"        }\r\n"
-"      };\r\n"
-"\r\n"
-"      count_up_btn.onclick = function (){\r\n"
-"          if(count_value < 4){\r\n"
-"            count_value += 1;\r\n"
-"            \r\n"
-"            show_led_icon();\r\n"
-"            \r\n"
-"            var nowcnt = document.getElementById('timer_count');\r\n"
-"            nowcnt.value = count_value;\r\n"
-"          }\r\n"
-"      };\r\n"
-"\r\n"
-"      count_up_btn.onmousedown = function() {\r\n"
-"      }\r\n"
-"\r\n"
-"      count_up_btn.onmouseup = function() {\r\n"
-"      }\r\n"
-"\r\n"
-"    }\r\n"
 "  </script>\r\n"
 "</head>\r\n"
 "<body>      \r\n"
 "  <p id = 'display'>Not connected</p>\r\n"
-"          \r\n"
-"  <div class='parent'>\r\n"
-"    <div class='child powerpnl'>\r\n"
-"      <form method='post' id='power_form' action='./power' target='tif0' >\r\n"
-"        <iframe src='javascript: false;' style='display: none;' name='tif0' id='tif0'></iframe>\r\n"
-"        <input type='checkbox' name='poweron' class='checkboxyz' id='poweron' onchange='document.forms.power_form.submit();'>\r\n"
-"        <label for='poweron'>\r\n"
-"          <svg class='power-icon' version='1.1' id='_x32_' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'\r\n"
-"            viewBox='-12.5 0 100 100' style='' xml:space='preserve' preserveAspectRatio='none'>\r\n"
-"            <g>\r\n"
-"              <path\r\n"
-"                class='st0'\r\n"
-"                d='M 21.648156,5.4768425 A 11.90625,11.90625 0 0 1 24.229107,18.452157 11.90625,11.90625 0 0 1 13.229166,25.802083 11.90625,11.90625 0 0 1 2.2292255,18.452158 11.90625,11.90625 0 0 1 4.8101758,5.476843'\r\n"
-"                style='stroke-width:3;stroke-linecap:round;' />\r\n"
-"             <path\r\n"
-"                class='st0'\r\n"
-"                d='m 13.229167,3.3125 c 0,9.260417 0,9.260417 0,9.260417'\r\n"
-"                style='stroke-width:3;stroke-linecap:round;' />\r\n"
-"            </g>\r\n"
-"          </svg>\r\n"
-"        </label>\r\n"
-"      </form>\r\n"
-"    </div>\r\n"
-"\r\n"
-"    <div class='child operationpnl'>\r\n"
-"      <form method='post' id='operation_form' action='./operation' target='tif1' >\r\n"
-"        <iframe src='javascript: false;' style='display: none;' name='tif1' id='tif1'></iframe>\r\n"
-"        <div>\r\n"
-"          <input type='submit' name='operation' value='start' class='button' ><br/>\r\n"
-"          <input type='submit' name='operation' value='stop' class='button' >\r\n"
-"        </div>\r\n"
-"      </form>\r\n"
-"    </div>\r\n"
-"    <div class='child child04'>\r\n"
-"        <input type='submit' name='timer' value='set' class='button' id='btn_count_down' />\r\n"
-"    </div>\r\n"
 "\r\n"
 "    <div class='child controlpnl'>\r\n"
 "      <form method='post' id='control_form' action='./control' target='tif3' >\r\n"
 "        <iframe src='javascript: false;' style='display: none;' name='tif3' id='tif3'></iframe>\r\n"
 "        <div class='controlpnl' >\r\n"
-"          <input type='submit' name='dir' value='forward' class='button' ><br/>\r\n"
+"          <input type='submit' name='dir' value='lforward' class='button' >\r\n"
+"          <input type='submit' name='dir' value='forward' class='button' >\r\n"
+"          <input type='submit' name='dir' value='rforward' class='button' ><br/>\r\n"
 "          <input type='submit' name='dir' value='left' class='button' >\r\n"
 "          <input type='submit' name='dir' value='stop' class='button' >\r\n"
 "          <input type='submit' name='dir' value='right' class='button' ><br/>\r\n"
+"          <input type='submit' name='dir' value='lback' class='button' >\r\n"
 "          <input type='submit' name='dir' value='back' class='button' >\r\n"
+"          <input type='submit' name='dir' value='rback' class='button' ><br/>\r\n"
+"          <input type='submit' name='dir' value='rrotate' class='button' >\r\n"
+"          <input type='submit' name='dir' value='lrotate' class='button' >\r\n"
 "        </div>\r\n"
 "      </form>\r\n"
 "    </div>\r\n"
-"\r\n"
-"    <div class='child configpnl'>\r\n"
-"      <form method='post' id='config_form' action='./get_config' >\r\n"
-"        <input type='submit' name='config_open' value='open' class='button' />\r\n"
-"      </from>\r\n"
-"    </div>\r\n"
-"    %YOU_HAVE_CONTROL%\r\n"
-"  </div>\r\n"
-"    \r\n"
 "</body>\r\n"
 "</html>\r\n";
 
@@ -336,11 +253,9 @@ void loop()
     //Serial.println(rover_c.y_val);
     //Serial.println(rover_c.yaw_val);
     drawScreen();
+    ws.text(g_client_id,String(-1));
     pre_time = millis();
   }
-
-  
-
   //M5.update();
 }
 
@@ -363,12 +278,12 @@ void handleRoot(AsyncWebServerRequest *request) {
 void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
 
   if(type == WS_EVT_CONNECT){
-
     Serial.println("Websocket client connection received");
+    g_client_id = client->id();
 
   } else if(type == WS_EVT_DISCONNECT){
     Serial.println("Client disconnected");
-
+    g_client_id = -1;
   } 
 }
 
@@ -381,11 +296,23 @@ void handle_control(AsyncWebServerRequest *request)
   if (s == "forward") {
       rover_c.move(0,50,0);
   } else if (s == "left") {
-      rover_c.turn(-50,0);
+      rover_c.move(50,0,0);
   } else if (s == "right") {
-      rover_c.turn(50,0);
+      rover_c.move(-50,0,0);
   } else if (s == "back") {
       rover_c.move(0,-50,0);
+  } else if (s == "lforward") {
+      rover_c.move(50,50,0);
+  } else if (s == "rforward") {
+      rover_c.move(-50,50,0);
+  } else if (s == "lback") {
+      rover_c.move(50,-50,0);
+  } else if (s == "rback") {
+      rover_c.move(-50,-50,0);
+  } else if (s == "lrotate") {
+      rover_c.turn(30,0);
+  } else if (s == "rrotate") {
+      rover_c.turn(-30,0);
   } else if (s == "stop") {
       rover_c.stop();
   } else {
